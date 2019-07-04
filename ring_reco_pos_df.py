1b00aa560d4577376d6d9986a956f86b79e492f2
Paola Ferrario <paola.ferrario@gmail.com>
2019-07-04 17:09:49 +0200

    Add inequality to check electron energy + simplify code.

--- ring_reco_pos_df.py
+++ ring_reco_pos_df.py
@@ -10,60 +10,69 @@ from   antea.utils.table_functions import load_rpos
 
 import utils.histo_functions as myhf
 
+def lower_or_equal(f1, f2, allowed_error=1.e-6):
+    return f1 <= f2 + allowed_error
+
+
+def greater_or_equal(f1, f2, allowed_error=1.e-6):
+    return f1 >= f2 - allowed_error
+
+
 def from_cartesian_to_cyl(pos):
     cyl_pos = np.array([np.sqrt(pos[0]**2+pos[1]**2), np.arctan2(pos[1], pos[0]), pos[2]]).transpose()
     return cyl_pos
+
 
 def from_cartesian_to_cyl_v(pos):
     cyl_pos = np.array([np.sqrt(pos[:,0]**2+pos[:,1]**2), np.arctan2(pos[:,1], pos[:,0]), pos[:,2]]).transpose()
     return cyl_pos
 
+
 def find_SiPMs_over_thresholds(df, threshold):
-    
-    tot_charges_df = df.groupby(['event_id','sensor_id'])[['charge']].sum()    
+
+    tot_charges_df = df.groupby(['event_id','sensor_id'])[['charge']].sum()
     return tot_charges_df[tot_charges_df.charge > threshold].reset_index()
 
-def find_closest_sipm(given_pos, df):
+def find_closest_sipm(given_pos, sns_positions):
     ### Find the closest SiPM to the true average point
-    sipms = DataSiPM_idx.loc[df.sensor_id]
-    positions = np.array([sipms.X, sipms.Y, sipms.Z]).transpose()
-    subtr = np.subtract(given_pos, positions)
+    subtr = np.subtract(given_pos, sns_positions)
 
     distances = [np.linalg.norm(d) for d in subtr]
     min_dist = np.min(distances)
     min_sipm = np.isclose(distances, min_dist)
     closest_sipm = sipms[min_sipm]
-    
+
     return closest_sipm
 
 
 def find_given_particle_hits(p_ids, hits):
     return  hits[hits.particle_id.isin(p_ids)]
 
+
 def assign_sipms_to_gammas(waveforms, true_pos, DataSiPM_idx):
-        
+
     sipms = DataSiPM_idx.loc[waveforms.sensor_id]
-    sns_positions = np.array([sipms.X, sipms.Y, sipms.Z]).transpose() 
+    sns_positions = np.array([sipms.X, sipms.Y, sipms.Z]).transpose()
     sns_charges   = waveforms.charge
 
-    sns_closest_pos = [np.array([find_closest_sipm(pos, waveforms).X.values, find_closest_sipm(pos, waveforms).Y.values, \
-                                 find_closest_sipm(pos, waveforms).Z.values]).transpose()[0] for pos in true_pos]
+    sns_closest_pos = [np.array([find_closest_sipm(pos, sns_positions).X.values, find_closest_sipm(pos, sns_positions).Y.values, \
+                                 find_closest_sipm(pos, sns_positions).Z.values]).transpose()[0] for pos in true_pos]
 
     q1, q2     = [], []
     pos1, pos2 = [], []
-    
-    closest_pos = sns_closest_pos[0] ## I keep the first one, which always exists.
+
+    closest_pos = sns_closest_pos[0] ## I look at the first one, which always exists.
     ### The sensors on the same semisphere are grouped together, and those on the opposite side, too, only
     ### if two 511-keV interactions have been detected.
     for sns_pos, charge in zip(sns_positions, sns_charges):
-        scalar_prod =  sum(a*b for a, b in zip(sns_pos, closest_pos)) 
+        scalar_prod = sum(a*b for a, b in zip(sns_pos, closest_pos))
         if scalar_prod > 0.:
             q1.append(charge)
             pos1.append(sns_pos)
         elif len(sns_closest_pos) == 2:
             q2.append(charge)
             pos2.append(sns_pos)
-            
+
     return q1, q2, pos1, pos2
 
 ### read sensor positions from database
@@ -114,41 +123,42 @@ for ifile in range(start, start+numb):
     particles = pd.read_hdf(file_name, 'MC/particles')
     hits      = pd.read_hdf(file_name, 'MC/hits')
 
-    r_thr, z_phi_thr = 4, 4
+    r_thr, z_phi_thr = max_cut, max_cut
     sel_df_r      = find_SiPMs_over_thresholds(full_sns_response, threshold=r_thr)
     sel_df_z_phi  = find_SiPMs_over_thresholds(full_sns_response, threshold=z_phi_thr)
 
     events = particles.event_id.unique()
 
     for evt in events[:]:
-    
+
         evt_parts = particles[particles.event_id == evt]
         evt_hits  = hits[hits.event_id           == evt]
         primaries = evt_parts[evt_parts.primary == True]
         #tot_energy = hits[hits.event_id == evt].energy.sum()
-    
+
         sel_volume   = (evt_parts.initial_volume == 'ACTIVE') & (evt_parts.final_volume == 'ACTIVE')
         sel_name     = evt_parts.name == 'e-'
         sel_vol_name = evt_parts[sel_volume & sel_name]
-    
+
         ids      = sel_vol_name.particle_id.values
         #sel_hits             = evt_hits[evt_hits.particle_id.isin(ids)]
         sel_hits = find_given_particle_hits(ids, evt_hits)
         energies = sel_hits.groupby(['particle_id'])[['energy']].sum()
         energies = energies.reset_index()
         #print(energies)
-        energy_sel  = energies[np.isclose(energies.energy, 0.476443, atol=1.e-6)] 
+        #energy_sel  = energies[np.isclose(energies.energy, 0.476443, atol=1.e-6)]
+        energy_sel  = energies[greater_or_equal(energies.energy, 0.476443, allowed_error=1.e-6)]
         #print(energy_sel)
         sel_vol_name_e  = sel_vol_name[sel_vol_name.particle_id.isin(energy_sel.particle_id)]
         #print(sel_vol_name_e)
 
         sel_all         = sel_vol_name_e[sel_vol_name_e.mother_id.isin(primaries.particle_id.values)]
         if len(sel_all) == 0: continue
-    
+
             ### now that I have selected the event, let's calculate the true position
         ids      = sel_all.particle_id.values
         sel_hits = find_given_particle_hits(ids, evt_hits)
-    
+
         sel_hits = sel_hits.groupby(['particle_id'])
         true_pos = []
         for _, df in sel_hits:
@@ -156,10 +166,10 @@ for ifile in range(start, start+numb):
             true_pos.append(np.average(hit_positions, axis=0, weights=df.energy))
                 #    print('*** True position(s) ***')
                 #    print(true_pos)
-        
+
         waveforms = sel_df_r[sel_df_r.event_id == evt]
         if len(waveforms) == 0: continue
-        
+
         q1, q2, pos1, pos2 = assign_sipms_to_gammas(waveforms, true_pos, DataSiPM_idx)
         var_phi1 = var_phi2 = None
         if len(pos1) > 0:
@@ -167,20 +177,20 @@ for ifile in range(start, start+numb):
             mean_phi = np.average(pos1_phi, weights=q1)
             var_phi1 = np.average((pos1_phi-mean_phi)**2, weights=q1)
         if len(pos2) > 0:
-            pos2_phi = from_cartesian_to_cyl_v(np.array(pos2))[:,1]   
+            pos2_phi = from_cartesian_to_cyl_v(np.array(pos2))[:,1]
             mean_phi = np.average(pos2_phi, weights=q2)
             var_phi2 = np.average((pos2_phi-mean_phi)**2, weights=q2)
 
         waveforms_z_phi = sel_df_z_phi[sel_df_z_phi.event_id == evt]
         if len(waveforms) == 0: continue
-        
+
         q1, q2, pos1, pos2 = assign_sipms_to_gammas(waveforms_z_phi, true_pos, DataSiPM_idx)
 
-        if len(pos1) > 0 and var_phi1:  
-            reco_r    = Rpos(np.sqrt(var_phi1)).value 
+        if len(pos1) > 0 and var_phi1:
+            reco_r    = Rpos(np.sqrt(var_phi1)).value
             reco_cart = np.average(pos1, weights=q1, axis=0)
             reco_phi  = np.arctan2(reco_cart[1], reco_cart[0])
-                                
+
             reco_x1.append(reco_r * np.cos(reco_phi))
             reco_y1.append(reco_r * np.sin(reco_phi))
             reco_z1.append(reco_cart[2])
@@ -199,12 +209,12 @@ for ifile in range(start, start+numb):
             true_z1.append(1.e9)
             photo_response1.append(1.e9)
             touched_sipms1.append(1.e9)
-        
-        if len(pos2) > 0 and var_phi2: 
-            reco_r    = Rpos(np.sqrt(var_phi2)).value 
+
+        if len(pos2) > 0 and var_phi2:
+            reco_r    = Rpos(np.sqrt(var_phi2)).value
             reco_cart = np.average(pos2, weights=q2, axis=0)
             reco_phi  = np.arctan2(reco_cart[1], reco_cart[0])
-                                    
+
             reco_x2.append(reco_r * np.cos(reco_phi))
             reco_y2.append(reco_r * np.sin(reco_phi))
             reco_z2.append(reco_cart[2])
@@ -222,7 +232,7 @@ for ifile in range(start, start+numb):
             true_y2.append(1.e9)
             true_z2.append(1.e9)
             photo_response2.append(1.e9)
-            touched_sipms2.append(1.e9)  
+            touched_sipms2.append(1.e9)
 
 
 a_true_x1 = np.array(true_x1)
