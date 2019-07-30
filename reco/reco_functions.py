3ac3c7ac1178b1867ee80f5d754cfcd1525417c0
Paola Ferrario <paola.ferrario@gmail.com>
2019-07-30 11:02:20 +0200

    Add dependency on charge threshold for coincidences, too.

--- reco/reco_functions.py
+++ reco/reco_functions.py
@@ -196,9 +196,9 @@ def find_reco_pos(current_charge: Dict[int, Dict[int, Waveform]], r_threshold: f
     return reco_r, reco_phi, reco_z
 
 
-def select_coincidences(current_charge: Dict[int, Dict[int, Waveform]], charge_range: Tuple[float, float], sens_pos: Dict[int, Tuple[float, float, float]], particle_dict: Dict[int, Sequence[MCParticle]]) -> (Sequence[Tuple[float, float, float, float]], Sequence[Tuple[float, float, float, float]]):
+def select_coincidences(current_charge: Dict[int, Dict[int, Waveform]], threshold, charge_range: Tuple[float, float], sens_pos: Dict[int, Tuple[float, float, float]], particle_dict: Dict[int, Sequence[MCParticle]]) -> (Sequence[Tuple[float, float, float, float]], Sequence[Tuple[float, float, float, float]]):
 
-    sns_over_thr, charges_over_thr = find_SiPMs_over_thresholds(current_charge, threshold=2)
+    sns_over_thr, charges_over_thr = find_SiPMs_over_thresholds(current_charge, threshold)
     if len(sns_over_thr) == 0:
         return [], [], None, None
 
